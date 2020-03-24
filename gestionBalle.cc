8d7b98aa25e7de18a6b28a887ca096c711c03b0b
Frenchy31 <aubin.puyoou@gmail.com>
2020-03-24 10:35:13 +0100

    Commit pong complet

---
+++ gestionBalle.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include "SDL.h"
+#include "SDL_image.h"
+#include<SDL/SDL_ttf.h> // inclusion de la gestion de l'affichage des fontes
+#include<sstream> // pour conversion du score
+
+#include "gestionBalle.h"
+using namespace std;
+
+
---
+++ gestionBalle.h
@@ -0,0 +1,10 @@
+#ifndef GESTIONBALLE_H_INCLUDED
+#define GESTIONBALLE_H_INCLUDED
+
+
+void mouvementBalle(Balle &balle, Joueur &joueur1, Joueur &joueur2, int hauteurEcran, int largeurEcran,
+	bool &pause, ostringstream &messageJ1, ostringstream &messageJ2) ;
+
+#endif // GESTIONBALLE_H_INCLUDED
+
+
